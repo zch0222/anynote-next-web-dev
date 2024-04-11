5216ab93cf8bfdd14fb9d0856db21a1c8943dadb
称霸幼儿园 <zhengchenghao@chengbayoueryuandeMac-mini.local>
2024-04-11 22:09:24 +0800

    feat: ICP

--- src/components/home/HomeFooter/index.tsx
+++ src/components/home/HomeFooter/index.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
                 <span className={'span-copy'}>&copy; 学习随记 &nbsp;&nbsp;</span>
                 <span className={'span-copy'}>ICP备案号：</span>
                 <Link target={'_blank'} href={'https://beian.miit.gov.cn/#/Integrated/index'}
-                      style={{fontSize: 14, color: '#8a8f8d'}}>浙ICP备2023028046号-1</Link>
+                      style={{fontSize: 14, color: '#8a8f8d'}}>{process.env.NEXT_PUBLIC_ICP}</Link>
             </div>
         </>
     )
