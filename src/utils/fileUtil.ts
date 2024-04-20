1dad254c2253333af7569ff1cbfff7040695330a
称霸幼儿园 <yxlmzch0222@gmail.com>
2024-04-21 01:00:41 +0800

    fix: hash

--- src/utils/fileUtil.ts
+++ src/utils/fileUtil.ts
@@ -1,5 +1,5 @@
 import * as crypto from 'crypto';
 
 export function calculateFileHash(reader: FileReader, type: "sha256" | "sha512" = "sha256"): string {
-    return crypto.createHash(type).digest("hex")
+    return crypto.createHash(type).update(new Uint8Array(reader.result)).digest("hex")
 }
